f1c15c719ac7cf39d912dcdd4cd348701e0bc392
Suneth Tissera <89357504+tisserasuneth@users.noreply.github.com>
2024-08-30 23:08:23 -0400

    (monogram) - unique mongoram icon

--- app/components/monogram/monogram.jsx
+++ app/components/monogram/monogram.jsx
@@ -10,15 +10,15 @@ export const Monogram = forwardRef(({ highlight, className, ...props }, ref) => 
     <svg
       aria-hidden
       className={classes(styles.monogram, className)}
-      width="48"
-      height="29"
-      viewBox="0 0 48 29"
+      width="50"
+      height="50"
+      viewBox="0 0 50 50"
       ref={ref}
       {...props}
     >
       <defs>
         <clipPath id={clipId}>
-          <path d="M0 0h6.5a6 6 0 0 1 5.2 3.1L19.4 17l4-9L19 0h6.5a6 6 0 0 1 5.2 3.1L39.5 19 35 29 24.5 10 16 29 0 0Zm46.7 2.8A2 2 0 0 0 45 0h-7l5.5 10 3.2-7.2Z" />
+          <path d="M15 1 L40 3 L40 10 L70 40 L40 30 L12 20 L1 40 L20 30 Z" />
         </clipPath>
       </defs>
       <rect clipPath={`url(#${clipId})`} width="100%" height="100%" />
