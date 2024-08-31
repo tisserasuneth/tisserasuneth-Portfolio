3eb799678073f20d36a9d0ad3b8205c3b57dc11a
Suneth Tissera <89357504+tisserasuneth@users.noreply.github.com>
2024-08-30 22:55:43 -0400

    (routes/home) - updating sphere colors

--- app/components/footer/footer.jsx
+++ app/components/footer/footer.jsx
@@ -7,12 +7,12 @@ import styles from './footer.module.css';
 export const Footer = ({ className }) => (
   <footer className={classes(styles.footer, className)}>
     <Text size="s" align="center">
+    <Link secondary className={styles.link} href="https://github.com/tisserasuneth" target="_self">
       <span className={styles.date}>
         {`© ${new Date().getFullYear()} ${config.name}.`}
       </span>
-      <Link secondary className={styles.link} href="https://github.com/hamishMW" target="_self">
-        Forked from Hamish Williams.
-      </Link>
+    </Link>
+      Forked from Hamish Williams.
     </Text>
   </footer>
 );
